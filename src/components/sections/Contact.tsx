1cbd1ca05abc904b6b8c4a661203ad1c92730dfb
isakidev <gaspar.c.developer@gmail.com>
2025-02-04 20:23:36 -0300

    feat: create contact section

---
+++ src/components/sections/Contact.tsx
@@ -0,0 +1,13 @@
+import { WrapperLayout } from "../layout/WrapperLayout"
+import { MapIframe } from "../ui/MapIframe"
+
+export const Contact = () => {
+  return (
+    <WrapperLayout>
+      <h2>Contacto</h2>
+
+      <MapIframe />
+
+    </WrapperLayout>
+  )
+}
