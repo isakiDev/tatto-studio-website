57e92a61dfa51175e68bfc6ca55e0ff3a49e449a
isakidev <gaspar.c.developer@gmail.com>
2025-02-04 20:23:05 -0300

    feat: update some styles

--- src/components/layout/WrapperLayout.tsx
+++ src/components/layout/WrapperLayout.tsx
@@ -2,8 +2,8 @@ import { ReactNode } from "react"
 
 export const WrapperLayout = ({ children }: { children?: ReactNode }) => {
   return (
-    <div className="max-w-[1000px] mx-auto">
+    <section className="max-w-[900px] mx-auto px-4 sm:px-0 text-center w-full">
       {children}
-    </div>
+    </section>
   )
 }
