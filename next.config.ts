c2f4c4eccf396ccad822acadd6cdd1a3cc68f54f
isakidev <gaspar.c.developer@gmail.com>
2025-02-04 20:20:27 -0300

    chore: config some files

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.tattoorockers.cl'
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'flowbite.s3.amazonaws.com'
       }
     ]
   }
