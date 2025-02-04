7f397d46c242c31fbdebb3b35c4bdf61ea4c1ea2
isakidev <gaspar.c.developer@gmail.com>
2025-02-04 20:25:04 -0300

    feat: create button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,6 +1,6 @@
 export const Button = () => {
   return (
-    <button className="px-4 py-2 hover:border-amber-600 transition-colors duration-300 border uppercase">
+    <button className="px-4 py-2 hover:border-amber-600 transition-colors duration-300 border uppercase font-semibold">
       AGENDA TU HORA
     </button>
   )
